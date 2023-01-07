8c2cf23b31d9a57f7fe41b4d4b4d3731e4b99d94
Tien <ngotienpts@gmail.com>
2023-01-07 12:00:40 +0700

    7/1

---
+++ js/main.js
@@ -0,0 +1,115 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // back top
+  var backTop = document.querySelector("#back-top");
+
+  // width document
+  var widthDoc = document.querySelector("body");
+
+  // fancybox
+  var fancyboxes = document.querySelectorAll(".fancybox-full");
+
+  const app = {
+    // su ly cac su kien
+    handleEvent: function () {
+      const _this = this;
+
+      // when click back top
+      if (backTop) {
+        backTop.onclick = function () {
+          document.body.scrollTop = 0;
+          document.documentElement.scrollTop = 0;
+        };
+      }
+
+      // hide cac element khi click ra ngoai
+      // document.addEventListener("click", function (e) {});
+    },
+
+    // slide topic list
+    slideToppicList: function () {
+      $(".topic-list").slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      });
+    },
+    // sticky bar home 1
+    stickyHome1: function () {
+      $(".leftSidebar-1,.rightSidebar-1").theiaStickySidebar({
+        additionalMarginTop: 60,
+      });
+    },
+    // fancybox
+    fancybox: function () {
+      if (fancyboxes) {
+        fancyboxes.forEach(function (fancybox) {
+          $(".fancybox-full a").fancybox();
+        });
+      }
+    },
+    // scroll top
+    scrollFunc: function () {
+      if (backTop) {
+        if (
+          document.body.scrollTop > 300 ||
+          document.documentElement.scrollTop > 300
+        ) {
+          backTop.style.opacity = 1;
+          backTop.style.visibility = "visible";
+        } else {
+          backTop.style.opacity = 0;
+          backTop.style.visibility = "hidden";
+        }
+      }
+    },
+    // window scroll
+    windowScroll: function () {
+      var _this = this;
+      window.onscroll = function () {
+        // scroll top
+        _this.scrollFunc();
+      };
+    },
+    // khoi tao function start
+    start: function () {
+      // su ly cac su kien
+      this.handleEvent();
+      // window scroll
+      this.windowScroll();
+      // slide topic list
+      // this.slideToppicList();
+      // sticky bar home 1
+      // this.stickyHome1();
+      // fancybox
+      this.fancybox();
+    },
+  };
+
+  app.start();
+});
